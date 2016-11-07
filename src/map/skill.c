eca31482052e20016d4dba9fb252f4ebb728860b
hurtsky <hurtskyxda@gmail.com>
2016-11-07 09:09:53 +0800

    skill: don't stop player attack
    Signed-off-by: hurtsky <hurtskyxda@gmail.com>

--- src/map/skill.c
+++ src/map/skill.c
@@ -2046,9 +2046,10 @@ int skill_additional_effect(struct block_list* src, struct block_list *bl, uint1
 				!battle_check_range(src, tbl, skill_get_range2(src, skill, autospl_skill_lv, true)))
 				continue;
 
-			if (skill == AS_SONICBLOW)
-				pc_stop_attack(sd); //Special case, Sonic Blow autospell should stop the player attacking.
-			else if (skill == PF_SPIDERWEB) //Special case, due to its nature of coding.
+//			if (skill == AS_SONICBLOW)
+//				pc_stop_attack(sd); //Special case, Sonic Blow autospell should stop the player attacking.
+			
+			if (skill == PF_SPIDERWEB) //Special case, due to its nature of coding.
 				type = CAST_GROUND;
 
 			sd->state.autocast = 1;
