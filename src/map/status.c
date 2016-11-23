1b7d623e63e64048192d2f4dccb008619d5c00dd
hurtsky <hurtskyxda@gmail.com>
2016-11-23 09:17:48 +0800

    Revert "src:map:status: remove status immunity calculation"
    reverted from commit 0be51b919b512e667ce4fd5af3df6496e2bbcebe

--- src/map/status.c
+++ src/map/status.c
@@ -7746,11 +7746,8 @@ int status_get_sc_def(struct block_list *src, struct block_list *bl, enum sc_typ
 	switch (type) {
 		case SC_POISON:
 		case SC_DPOISON:
-			if (status->luk == 600)
-				return 0;
-/*			sc_def = status->vit*100;
- *			sc_def2 = status->luk*10 + status_get_lv(bl)*10 - status_get_lv(src)*10;
- */
+			sc_def = status->vit*100;
+			sc_def2 = status->luk*10 + status_get_lv(bl)*10 - status_get_lv(src)*10;
 			if (sd) {
 				// For players: 60000 - 450*vit - 100*luk
 				tick_def = status->vit*75;
@@ -7762,100 +7759,67 @@ int status_get_sc_def(struct block_list *src, struct block_list *bl, enum sc_typ
 			}
 			break;
 		case SC_STUN:
-			if (status->luk == 600)
-				return 0;
-/*
- *			sc_def = status->vit*100;
- *			sc_def2 = status->luk*10 + status_get_lv(bl)*10 - status_get_lv(src)*10;
- */
+			sc_def = status->vit*100;
+			sc_def2 = status->luk*10 + status_get_lv(bl)*10 - status_get_lv(src)*10;
 			tick_def2 = status->luk*10;
 			break;
 		case SC_SILENCE:
-			if (status->luk == 600)
-				return 0;
-/*
- *#ifndef RENEWAL
- *			sc_def = status->vit*100;
- *			sc_def2 = status->luk*10 + status_get_lv(bl)*10 - status_get_lv(src)*10;
- *#else
- *			sc_def = status->int_*100;
- *			sc_def2 = (status->vit + status->luk) * 5 + status_get_lv(bl)*10 - status_get_lv(src)*10;
- *#endif
- */
+#ifndef RENEWAL
+			sc_def = status->vit*100;
+			sc_def2 = status->luk*10 + status_get_lv(bl)*10 - status_get_lv(src)*10;
+#else
+			sc_def = status->int_*100;
+			sc_def2 = (status->vit + status->luk) * 5 + status_get_lv(bl)*10 - status_get_lv(src)*10;
+#endif
 			tick_def2 = status->luk*10;
 			break;
 		case SC_BLEEDING:
-			if (status->luk == 600)
-				return 0;
-/*
- *#ifndef RENEWAL
- *			sc_def = status->vit*100;
- *			sc_def2 = status->luk*10 + status_get_lv(bl)*10 - status_get_lv(src)*10;
- *#else
- *			sc_def = status->agi*100;
- *			sc_def2 = status->luk*10 + status_get_lv(bl)*10 - status_get_lv(src)*10;
- *#endif
- *			tick_def2 = status->luk*10;
- */
+#ifndef RENEWAL
+			sc_def = status->vit*100;
+			sc_def2 = status->luk*10 + status_get_lv(bl)*10 - status_get_lv(src)*10;
+#else
+			sc_def = status->agi*100;
+			sc_def2 = status->luk*10 + status_get_lv(bl)*10 - status_get_lv(src)*10;
+#endif
+			tick_def2 = status->luk*10;
 			break;
 		case SC_SLEEP:
-			if (status->luk == 600)
-				return 0;
-/*
- *#ifndef RENEWAL
- *			sc_def = status->int_*100;
- *			sc_def2 = status->luk*10 + status_get_lv(bl)*10 - status_get_lv(src)*10;
- *#else
- *			sc_def = status->agi*100;
- *			sc_def2 = (status->int_ + status->luk) * 5 + status_get_lv(bl)*10 - status_get_lv(src)*10;
- *#endif
- *			tick_def2 = status->luk*10;
- */
+#ifndef RENEWAL
+			sc_def = status->int_*100;
+			sc_def2 = status->luk*10 + status_get_lv(bl)*10 - status_get_lv(src)*10;
+#else
+			sc_def = status->agi*100;
+			sc_def2 = (status->int_ + status->luk) * 5 + status_get_lv(bl)*10 - status_get_lv(src)*10;
+#endif
+			tick_def2 = status->luk*10;
 			break;
 		case SC_STONE:
-			if (status->luk == 600)
-				return 0;
-/*
- *			sc_def = status->mdef*100;
- *			sc_def2 = status->luk*10 + status_get_lv(bl)*10 - status_get_lv(src)*10;
- *			tick_def = 0; // No duration reduction
- */
+			sc_def = status->mdef*100;
+			sc_def2 = status->luk*10 + status_get_lv(bl)*10 - status_get_lv(src)*10;
+			tick_def = 0; // No duration reduction
 			break;
 		case SC_FREEZE:
-			if (status->luk == 600)
-				return 0;
-/*
- *			sc_def = status->mdef*100;
- *			sc_def2 = status->luk*10 + status_get_lv(bl)*10 - status_get_lv(src)*10;
- */
+			sc_def = status->mdef*100;
+			sc_def2 = status->luk*10 + status_get_lv(bl)*10 - status_get_lv(src)*10;
 			tick_def2 = status_src->luk*-10; // Caster can increase final duration with luk
 			break;
 		case SC_CURSE:
-			if (status->luk == 600)
+			// Special property: immunity when luk is zero
+			if (status->luk == 0)
 				return 0;
-/*
- *			sc_def = status->luk*100;
- *			sc_def2 = status->luk*10 - status_get_lv(src)*10; // Curse only has a level penalty and no resistance
- */
+			sc_def = status->luk*100;
+			sc_def2 = status->luk*10 - status_get_lv(src)*10; // Curse only has a level penalty and no resistance
 			tick_def = status->vit*100;
 			tick_def2 = status->luk*10;
 			break;
 		case SC_BLIND:
-			if (status->luk == 600)
-				return 0;
-/*
- *			sc_def = (status->vit + status->int_)*50;
- *			sc_def2 = status->luk*10 + status_get_lv(bl)*10 - status_get_lv(src)*10;
- */
+			sc_def = (status->vit + status->int_)*50;
+			sc_def2 = status->luk*10 + status_get_lv(bl)*10 - status_get_lv(src)*10;
 			tick_def2 = status->luk*10;
 			break;
 		case SC_CONFUSION:
-			if (status->luk == 600)
-				return 0;
-/*
- *			sc_def = (status->str + status->int_)*50;
- *			sc_def2 = status_get_lv(src)*10 - status_get_lv(bl)*10 - status->luk*10; // Reversed sc_def2
- */
+			sc_def = (status->str + status->int_)*50;
+			sc_def2 = status_get_lv(src)*10 - status_get_lv(bl)*10 - status->luk*10; // Reversed sc_def2
 			tick_def2 = status->luk*10;
 			break;
 		case SC_DECREASEAGI:
