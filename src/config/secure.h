3597c70dc4eedea37835c3380868e445b8c3890e
hurtsky <hurtskyxda@gmail.com>
2016-12-04 08:35:39 +0800

    Revert "src:confi:secure: increase npc timeout"
    reverted from commit 63eea0f3f90368438987cf11fa120461d05092b0

--- src/config/secure.h
+++ src/config/secure.h
@@ -24,19 +24,19 @@
 + * Number of seconds after an 'input' field is displayed before invoking an idle timeout.
 + * Default: 180
  **/
-#define NPC_SECURE_TIMEOUT_INPUT 300
+#define NPC_SECURE_TIMEOUT_INPUT 180
 
 /**
 + * Number of seconds after a 'menu' is displayed before invoking an idle timeout.
 + * Default: 60
  **/
-#define NPC_SECURE_TIMEOUT_MENU 120
+#define NPC_SECURE_TIMEOUT_MENU 60
 
 /**
 + * Number of seconds after a 'next' button is displayed before invoking an idle timeout.
 + * Default: 60
  **/
-#define NPC_SECURE_TIMEOUT_NEXT 120
+#define NPC_SECURE_TIMEOUT_NEXT 60
 
 /**
  * (Secure) Optional NPC Dialog Timer
@@ -44,6 +44,6 @@
  * Minimum Interval Between timeout checks in seconds
  * Default: 1s
  **/
-#define SECURE_NPCTIMEOUT_INTERVAL 10
+#define SECURE_NPCTIMEOUT_INTERVAL 1
 
 #endif // _CONFIG_SECURE_H_
